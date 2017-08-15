cbf87f8b757d302b400a95bca332583e34c144eb
wfondrie <wfondrie@umaryland.edu>
2017-08-15 07:56:00 -0400

    updating plot themes

--- R/ggplotTheme.R
+++ R/ggplotTheme.R
@@ -3,7 +3,7 @@ coolTheme <- theme_bw() +
     theme(axis.line = element_line(color = "black"),
           axis.ticks = element_line(color = "black"),
           axis.text = element_text(color = "black"),
-          text = element_text(size = 12, color = "black"),
+          text = element_text(size = 8, color = "black"),
           panel.border = element_rect(color = "black"),
           panel.grid.minor = element_blank(),
           panel.grid.major = element_line(size = 1))
